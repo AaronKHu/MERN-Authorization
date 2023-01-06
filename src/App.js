d5ef51c362b30b28a95e0f2a310f7ab1a141109b
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2023-01-06 11:45:35 -1200

    Added user context

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import './App.css';
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import Register from "./Register";
 import Login from './Login';
+import UserContext from './UserContext';
+
+import {useState} from 'react';
 
 function App() {
+
+  const [email, setEmail] = useState('');
+
   return (
-      <BrowserRouter>
-        <div>
-          <Link to={'/'}>Home</Link> |
-          <Link to={'/login'}>Login</Link> |
-          <Link to={'/register'}>Register</Link>
-        </div>
-        <Switch>
-          <Route exact path={'/register'} component={Register} />
-          <Route exact path={'/login'} component={Login} />
-        </Switch>
-        <hr/>
-      </BrowserRouter>
+      <UserContext.Provider value={{email:''}}>
+        <BrowserRouter>
+          <div>
+            <Link to={'/'}>Home</Link> |
+            <Link to={'/login'}>Login</Link> |
+            <Link to={'/register'}>Register</Link>
+          </div>
+          <Switch>
+            <Route exact path={'/register'} component={Register} />
+            <Route exact path={'/login'} component={Login} />
+          </Switch>
+          <hr/>
+        </BrowserRouter>
+      </UserContext.Provider>
   );
 }
 
--- src/Register.js
+++ src/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
 
         const data = {email, password};
         axios.post('http://localhost:4000/register', data, {withCredentials:true}) //Allows us to send cookie from React app to API, local host 4000 is where our express app is running
-            .then(()=>{});
+            .then(response =>{});
     }
 
     return (
